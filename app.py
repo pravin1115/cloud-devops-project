3bc1e8eb2c0e23b5ce5d45cdfd7c1d2c676cee86
Pravin Jaybhaye <pravinjaybhaye6595@gmail.com>
2026-06-15 22:17:27 +0530

    Update application to version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Cloud Native DevOps Project Running Successfully!"
+    return "Cloud Native DevOps Project - Version 2 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
